919680d3a8456efb13a61bdd7f2d198121dc5999
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-01-24 06:35:16 +0100

    Update xchar.cpp

--- Include/xchar.cpp
+++ Include/xchar.cpp
@@ -8,42 +8,65 @@ namespace core
     public:
         xchar():data(nullptr) {}
         xchar(const char* ch):data(std::make_unique<char[]>(charlen(ch) + 1)) {
-            ss << data.get();
-            auto dlen = std::size(ss.str());
-            xmemcpy(data.get(), ch, charlen(ch), dlen);
+            strcpy(data.get(), ch);
         }
         xchar(std::string_view str) {
             auto len = str.length();
-            data = std::make_unique<char[]>(len);
-            ss << data.get();auto dlen = std::size(ss.str());
-            xmemcpy(data.get(), str.data(), len, dlen);
+            data = std::make_unique<char[]>(len + 1);
+            charcpy(data.get(), str.data(), len + 1);
         }
         xchar(char ch) {
-            std::string lc = &ch;
-            auto len = lc.length();
-            ss << data.get();
-            auto dlen = std::size(ss.str());
-            xmemcpy(data.get(), &lc, len, dlen);
-        }
-        xchar(int i) {
-            ss << i;
-            auto ndata = ss.str().c_str();
-            auto len = std::size(ss.str());
-            ss << data.get();
-            auto dlen = std::size(ss.str());
-            xmemcpy(data.get(), &ndata, len, dlen);
-        }
-        void append(std::initializer_list<const char*>& list) {
-            auto len = list.size();
-            ss << data.get();auto dlen = std::size(ss.str());
-            for (auto i : list) {
-                xmemcpy(data.get(), i, len, dlen);
+            try
+            {
+                data = std::make_unique<char[]>(2);
+                data[0] = ch;
+                data[1] = '\0';
+            }
+            catch (const std::exception& e)
+            {
+                std::cerr << e.what() << '\n';
             }
         }
-        friend std::ostream& operator<<(std::ostream& os, const xchar& xc) {
-            os << xc.data.get();
-            return os;
+        xchar(int i) {
+            std::string str = std::to_string(i);
+            data = std::make_unique<char[]>(str.length() + 1);
+            charcpy(data.get(), str.c_str(), str.length() + 1);
         }
+        template<typename... Args>
+        void append(Args&&... args) {
+            std::initializer_list<const char*> list{ std::forward<Args>(args)... };
+            std::string temp;
+            temp.append(data.get());
+            for (auto i : list) {
+                temp.append(i);
+            }
+            auto len = temp.length();
+            data = std::make_unique<char[]>(len + 1);
+            charcpy(data.get(), temp.c_str(), len + 1);
+        }
+
+        void charcpy(char* dest, const char* src, size_t destSize) {
+            if (dest == nullptr || src == nullptr) {
+                return;
+            }
+
+            if (src < dest && src + destSize > dest) {
+                // overlapping
+                return;
+            }
+
+            if (std::strlen(src) >= destSize) {
+                // destSize is not large enough
+                return;
+            }
+
+            size_t i;
+            for (i = 0; i < destSize && src[i] != '\0'; i++) {
+                dest[i] = src[i];
+            }
+            dest[i] = '\0';
+        }
+
         xchar& operator=(const xchar& other) {
             auto ndata = other.data.get();
             auto _data = data.get();
@@ -53,32 +76,35 @@ namespace core
             }
             return *this;
         }
+        void xmemcpy(void* dest, const void* src, size_t n, size_t dest_size) {
+            if (!dest || !src) throw std::invalid_argument("dest and src pointers must not be null");
+            if (n == 0) return;
+            if (dest == src) throw std::runtime_error("dest and src pointers must not be the same");
+            if (n > dest_size) throw std::length_error("n is larger than the size of dest buffer");
+            if (std::less<const void*>()(dest, src) && std::less<const void*>()(src, static_cast<const char*>(dest) + n))
+                throw std::runtime_error("dest and src memory regions overlap");
+            std::memmove(dest, src, n);
+        }
+        size_t charlen(const char** str) {
+            size_t length = 0;
+            while (**str++) {
+                length++;
+            }
+            return length;
+        }
+        size_t charlen(const char* str) {
+            size_t length = 0;
+            while (*str++) {
+                length++;
+            }
+            return length;
+        }
+        friend std::ostream& operator<<(std::ostream& os, const xchar& xc) {
+            os << xc.data.get();
+            return os;
+        }
     private:
         std::unique_ptr<char[]> data;
         std::stringstream ss;
     };// end of the xchar class
-    size_t charlen(const char** str) {
-        size_t length = 0;
-        while (**str++) {
-            length++;
-        }
-        return length;
-    }
-    size_t charlen(const char* str) {
-        size_t length = 0;
-        while (*str++) {
-            length++;
-        }
-        return length;
-    }
-    void xmemcpy(void* dest, const void* src, size_t n, size_t dest_size) {
-        if (!dest || !src) throw std::invalid_argument("dest and src pointers must not be null");
-        if (n == 0) return;
-        if (dest == src) throw std::runtime_error("dest and src pointers must not be the same");
-        if (n > dest_size) throw std::length_error("n is larger than the size of dest buffer");
-        if (std::less<const void*>()(dest, src) && std::less<const void*>()(src, static_cast<const char*>(dest) + n))
-            throw std::runtime_error("dest and src memory regions overlap");
-        std::memmove(dest, src, n);
-    }
-
 } // namespace core
