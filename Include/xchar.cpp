dbf96655f0875bcd465e23b8eb1a292969fd6f77
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-02-22 18:46:26 +0100

    Update xchar.cpp

--- Include/xchar.cpp
+++ Include/xchar.cpp
@@ -107,82 +107,101 @@ namespace core
 		return xchar_iterator(data + charlen(data));
 	}
 	xchar() : data(nullptr), m_capacity(0) {}
-    xchar(const char* ch) : m_capacity(charlen(ch) + 1) {
-        for(int i = 0; i < m_capacity;++i){
-            xmemcpy(data,(char*)ch[i],m_capacity,capacity()*2);
 
-        }     
-    }
-    xchar(std::string& str) : m_capacity(str.length() + 1) {
-        data = new char[m_capacity];
-         memmove(data, str.data(), m_capacity);
-    }
+	xchar(const char* ch) : m_capacity(charlen(ch) + 1) {
+		data = new char[m_capacity];
+		charcpy(data, ch, m_capacity);
+	}
 
-    xchar(int i) {
-        std::string str = std::to_string(i);
-        m_capacity = str.length() + 1;
-        data = new char[m_capacity];
-        memmove(data, str.c_str(), m_capacity);
-    }
+	xchar(std::string& str) : m_capacity(str.length() + 1) {
+		data = new char[m_capacity];
+		charcpy(data, str.data(), m_capacity);
+	}
 
-    xchar(char& ch) : m_capacity(2) {
-        data = new char[m_capacity];
-        data[0] =ch;
-        data[1] = '\0';
-    }
+	xchar(int i) {
+		std::string str = std::to_string(i);
+		m_capacity = str.length() + 1;
+		data = new char[m_capacity];
+		charcpy(data, str.c_str(), m_capacity);
+	}
 
-    xchar(const xchar& other) : m_capacity(other.m_capacity) {
-        data = new char[m_capacity];
-        xmemcpy(data, other.data, other.m_capacity,capacity()*2);
-    }
+	xchar(char& ch) : m_capacity(2) {
+		data = new char[m_capacity];
+		data[0] = ch;
+		data[1] = '\0';
+	}
 
-    xchar(xchar&& other) : data(other.data), m_capacity(other.m_capacity) {
-        other.data = nullptr;
-        other.m_capacity = 0;
-    }
+	xchar(const xchar& other) : m_capacity(other.m_capacity) {
+		data = new char[m_capacity];
+		charcpy(data, other.data, m_capacity);
+	}
 
-    ~xchar() {
-        delete[] data;
-    }
-    size_t copy(char* dest, size_t count, size_t pos = 0) const {
-            size_t data_len = std::strlen(data);
-            if (pos > data_len) {
-                return 0;
-            }
-            size_t len = std::min(data_len - pos, count);
-            charcpy(dest, data + pos, len);
-            return len;
-        }
-    size_t capacity() const {
-        return m_capacity;
-    }
+	xchar(xchar&& other) : data(other.data), m_capacity(other.m_capacity) {
+		other.data = nullptr;
+		other.m_capacity = 0;
+	}
 
-    const char* get() const {
-        return data;
-    }
+	friend std::ostream& operator<<(std::ostream& os, const xchar& x) {
+		return os << x.data;
+	}
 
-    size_t charlen(const char* str) {
-        size_t length = 0;
-        while (*str++) {
-            length++;
-        }
-        return length;
-    }
-    void swap(xchar& other) {
-            std::swap(data, other.data);
-        }
-    void reserve(size_t new_capacity) {
-            if (new_capacity > m_capacity) {
-                char* new_data(new char[new_capacity]);
-                std::memcpy(new_data, data,capacity()*2);
-                data = std::move(new_data);
-                m_capacity = new_capacity;
-            }
-        }
+	friend std::istream& operator>>(std::istream& is, xchar& x) {
+		std::string temp;
+		is >> temp;
+		x.reserve(2);
+		x.charcpy(x.data, temp.c_str(), x.m_capacity);
+		return is;
+	}
+
+	~xchar() {
+		delete[] data;
+	}
+
+	void swap(xchar& other) {
+		std::swap(data, other.data);
+		std::swap(m_capacity, other.m_capacity);
+	}
+
+	size_t capacity() const {
+		return m_capacity;
+	}
+
+	const char* get() const {
+		return data;
+	}
+
+	size_t charlen(const char* str) {
+		size_t length = 0;
+		while (*str++) {
+			length++;
+		}
+		return length;
+	}
+
+	void reserve(size_t new_capacity) {
+		if (new_capacity > m_capacity) {
+			char* new_data = new char[new_capacity];
+			charcpy(new_data, data, m_capacity);
+			delete[] data;
+			data = new_data;
+			m_capacity = new_capacity;
+		}
+	}
+
+	size_t copy(char* dest, size_t count, size_t pos = 0) const {
+		size_t data_len = std::strlen(data);
+		if (pos > data_len) {
+			return 0;
+		}
+		size_t len = std::min(data_len - pos, count);
+		charcpy(dest, data + pos, len);
+		return len;
+	}
+
 private:
-    char* data;
-    size_t m_capacity;
-    static constexpr size_t npos = std::numeric_limits<size_t>::max();
+	char* data;
+	size_t m_capacity;
+	static constexpr size_t npos = std::numeric_limits<size_t>::max();
   void xmemcpy(void* dest, const void* src, size_t n, size_t dest_size)
         {
             if (!dest || !src)
@@ -222,35 +241,4 @@ private:
         }
     };
 }// namespace core
-namespace LV {
-    class xchar_view {
-        using xchar = core::xchar;
-    public:
-        xchar_view(const xchar& str): data_(str.get()), length_(str.size()) {}
-        xchar_view(const char* str): data_(str), length_(std::strlen(str)) {}
-        const char* get() { return data_; }
-        void remove_prefix(size_t n) {
-            data_ += n;
-            length_ -= n;
-        }
-        void remove_suffix(size_t n) {
-            length_ -= n;
-        }
-        void swap(xchar_view& other) {
-            std::swap(data_, other.data_);
-            std::swap(length_, other.length_);
-        }
-        const char* begin() const { return data_; }
-        const char* end() const { return data_ + length_; }
-
-        xchar_view& operator=(const xchar& str) {
-            data_ = str.get();
-            length_ = str.size();
-            return *this;
-        }
-    private:
-        const char* data_;
-        size_t length_;
-    };// end for xchar_view
-}
 #endif
